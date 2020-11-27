43a27e0b61fe43df6ad844f4290f6e814031aa16
MohanramAnbukkarasu <44049406+MohanramAnbukkarasu@users.noreply.github.com>
2020-11-27 16:45:31 +0530

    Included sample to change the header text of the columns based on the culture settings

---
+++ OrderInfo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SfDataGrid_Demo
+{
+    public partial class OrderInfo : INotifyPropertyChanged
+    {
+        private int _OrderID;
+        private string _CustomerID;
+        private string _shipaddress;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OrderInfo"/> class.
+        /// </summary>
+        public OrderInfo()
+        {
+
+        }
+
+        /// <summary>
+        /// Gets or sets the order ID.
+        /// </summary>
+        /// <value>The order ID.</value>
+        [Display(Name = "Order ID")]
+        public int OrderID
+        {
+            get
+            {
+                return this._OrderID;
+            }
+            set
+            {
+                this._OrderID = value;
+                this.OnPropertyChanged("OrderID");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the customer ID.
+        /// </summary>
+        /// <value>The customer ID.</value>
+        [Display(Name = "Customer ID")]
+        public string CustomerID
+        {
+            get
+            {
+                return this._CustomerID;
+            }
+            set
+            {
+                this._CustomerID = value;
+                this.OnPropertyChanged("CustomerID");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the ShipAddress.
+        /// </summary>
+        /// <value>The ShipAddress.</value>
+        [Display(Name = "Ship Country")]
+        public string ShipCountry
+        {
+            get
+            {
+                return this._shipaddress;
+            }
+            set
+            {
+                this._shipaddress = value;
+                this.OnPropertyChanged("ShipCountry");
+
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
